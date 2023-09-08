c887c511617a21d202f5f12658f014290af0467c
AdityaU74 <upadhyay.aditya405@gmail.com>
2023-09-08 16:06:16 +0530

    fixed security issues

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
+const helmet = require('helmet');
 
 const session = require('express-session')
 const MongoStore = require('connect-mongo');
@@ -20,6 +21,8 @@ const MongoStore = require('connect-mongo');
 const userRoutes = require('./routes/users');
 const collegesRoutes = require('./routes/colleges.js');
 const reviewsRoutes = require('./routes/reviews.js');
+const mongoSanitize = require('express-mongo-sanitize');
+
 
 
 const app = express();
@@ -55,6 +58,12 @@ app.use(express.urlencoded({extended:true}))
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use(
+  mongoSanitize({
+    replaceWith: '_',
+  }),
+);
+
 const store = MongoStore.create({
   mongoUrl: dbUrl,
   touchAfter: 24 * 60 * 60,
@@ -65,6 +74,7 @@ const store = MongoStore.create({
 
 const sessionConfig = {
   store,
+  name: 'session',
   secret: 'thisshouldbesecret',
   resave: false,
   saveUninitialized: true,
@@ -76,6 +86,11 @@ const sessionConfig = {
 }
 app.use(session(sessionConfig))
 app.use(flash());
+app.use(
+  helmet({
+    contentSecurityPolicy: false
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -85,6 +100,9 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+  if (!['/login', '/'].includes(req.originalUrl)) {
+    req.session.returnTo = req.originalUrl;
+  }
   res.locals.currentUser = req.user;
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
